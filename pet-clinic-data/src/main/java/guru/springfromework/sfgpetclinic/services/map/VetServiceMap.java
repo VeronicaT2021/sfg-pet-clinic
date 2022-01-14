983c317402a105a225685e9b9e36b6a1c3496cba
VeronicaT2021 <veronica.tso2021@gmail.com>
2022-01-14 18:11:47 +0800

    implemented map based services, closes #6

---
+++ pet-clinic-data/src/main/java/guru/springfromework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springfromework.sfgpetclinic.services.map;
+
+import guru.springfromework.sfgpetclinic.model.Owner;
+import guru.springfromework.sfgpetclinic.model.Vet;
+import guru.springfromework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
