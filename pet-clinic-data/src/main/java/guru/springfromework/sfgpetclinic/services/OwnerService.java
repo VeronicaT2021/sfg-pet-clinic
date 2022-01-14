f2cd257a53fff472ac1247f8d239e798100d7d30
VeronicaT2021 <veronica.tso2021@gmail.com>
2022-01-14 10:04:35 +0800

    Implement Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/guru/springfromework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,15 @@
+package guru.springfromework.sfgpetclinic.services;
+
+import guru.springfromework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/guru/springfromework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package guru.springfromework.sfgpetclinic.services;
+
+import guru.springfromework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
