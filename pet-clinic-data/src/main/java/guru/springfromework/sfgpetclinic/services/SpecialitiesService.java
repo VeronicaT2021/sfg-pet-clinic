df2e0f7c28884a061a0074fbc8ddecaa07d80ca2
VeronicaT2021 <veronica.tso2021@gmail.com>
2022-01-16 14:19:26 +0800

    added Specialities Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/guru/springfromework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package guru.springfromework.sfgpetclinic.services;
+
+import guru.springfromework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
