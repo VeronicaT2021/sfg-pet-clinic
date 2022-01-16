0ccd81a1b2ff383402512cd7d964d796494b5940
VeronicaT2021 <veronica.tso2021@gmail.com>
2022-01-16 13:43:59 +0800

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/guru/springfromework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springfromework.sfgpetclinic.services;
+
+import guru.springfromework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
